b6a7cc4f985aa8d77a263b3cac672757dfa0c4db
Nanzhong Deng <nd2410@nyu.edu>
2024-11-22 23:23:42 -0500

    Animation_Section

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # Manim_Project
 Learn how to use Manim to depict math 
+
+## Render a scene
+In commind line: _manim -pql first_few_animations.py SquareToCircle_
+
+This command tells manim exactly which Scene is to be rendered, 
+in this case, it is SquareToCircle.  
+
+This is necessary because a single scene file may contain more than one scene.  
+
+Next, the flag -p tells manim to play the scene once it’s rendered, 
+and the -ql flag tells manim to render the scene in low quality. 
+
+The -ql flag (for low quality, 480p15) can be replaced by the -qh flag, 
+for high quality (1080p60). Can also add more flags, such as -s -ql or -s -qh. 
+You can also add -qm, -qh, -qp and -qk for medium (1280x720 30FPS), high (1920x1080 60FPS), 2k (2560x1440 60FPS) and 4k quality (3840x2160 60FPS), respectively.
+
+If your file contains multiple Scene classes, and you want to render them all, you can use the -a flag.     
+
+The -p flag plays the animation once it is rendered. If you want to open the file browser at the location of the animation instead of playing it, you can use the -f flag. You can also omit these two flags.
+### Save the last frame
+Adding the flag -s tells manim to save the last frame of the scene.   
+Saved frames can be found in themedia/images/first_few_animations folder.
+
+### Save Gif
+Finally, by default manim will output .mp4 files. If you want your animations in .gif format instead, use the --format gif flag. The output files will be in the same folder as the .mp4 files, and with the same name, but a different file extension.
--- first_few_animations.py
+++ first_few_animations.py
@@ -1,5 +1,11 @@
 from manim import *
 
+class CreateCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+        self.play(Create(circle))  # show the circle on screen
+
 class SquareToCircle(Scene):
     def construct(self):
         circle = Circle()  # create a circle
@@ -11,3 +17,93 @@ class SquareToCircle(Scene):
         self.play(Create(square))  # animate the creation of the square
         self.play(Transform(square, circle))  # interpolate the square into the circle
         self.play(FadeOut(square))  # fade out animation
+
+class SquareAndCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        circle.set_fill(PINK, opacity=0.5)  # set the color and transparency
+
+        square = Square()  # create a square
+        square.set_fill(BLUE, opacity=0.5)  # set the color and transparency
+
+        square.next_to(circle, RIGHT, buff=0.5)  # set the position
+        self.play(Create(circle), Create(square))
+
+
+class AnimatedSquareToCircle(Scene):
+    def construct(self):
+        circle = Circle()  # create a circle
+        square = Square()  # create a square
+
+        self.play(Create(square))  # show the square on screen
+        self.play(square.animate.rotate(PI / 4))  # rotate the square
+        self.play(Transform(square, circle))  # transform the square into a circle
+        self.play(
+            square.animate.set_fill(PINK, opacity=0.5)
+        )  # color the circle on screen
+
+
+class DifferentRotations(Scene):
+    def construct(self):
+        left_square = Square(color=BLUE, fill_opacity=0.7).shift(2 * LEFT)
+        right_square = Square(color=GREEN, fill_opacity=0.7).shift(2 * RIGHT)
+        self.play(
+            left_square.animate.rotate(PI), Rotate(right_square, angle=PI), run_time=2
+        )
+        self.wait()
+# show the shapes on screen
+
+class TwoTransforms(Scene):
+    def transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(Transform(a, b))
+        self.play(Transform(a, c))
+        self.play(FadeOut(a))
+
+    def replacement_transform(self):
+        a = Circle()
+        b = Square()
+        c = Triangle()
+        self.play(ReplacementTransform(a, b))
+        self.play(ReplacementTransform(b, c))
+        self.play(FadeOut(c))
+
+    def construct(self):
+        self.transform()
+        self.wait(0.5)  # wait for 0.5 seconds
+        self.replacement_transform()
+
+class TransformCycle(Scene):
+    def construct(self):
+        a = Circle()
+        t1 = Square()
+        t2 = Triangle()
+        self.add(a)
+        self.wait()
+        for t in [t1,t2]:
+            self.play(Transform(a,t))
+
+from manim import *
+
+class SectionUsage(Scene):
+    def construct(self):
+        # First section
+        self.next_section(name="Introduction", skip_animations=False)
+        circle = Circle()
+        circle.set_fill(PINK, opacity=0.5)
+        self.play(Create(circle))
+        self.play(circle.animate.shift(UP))
+
+        # Second section
+        self.next_section(name="Square Animation", skip_animations=False)
+        square = Square()
+        square.set_fill(BLUE, opacity=0.5)
+        self.play(Create(square))
+        self.play(square.animate.shift(DOWN))
+
+        # Third section
+        self.next_section(name="Final Positioning", skip_animations=False)
+        self.play(square.animate.rotate(PI / 2))
+        self.play(circle.animate.set_fill(WHITE), square.animate.set_fill(GREEN))
