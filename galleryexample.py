9bd1e02809fa1487f1a0918b4747ac5252691653
Nanzhong Deng <nd2410@nyu.edu>
2024-12-18 22:02:02 -0500

    updated gallery examples

--- galleryexample.py
+++ galleryexample.py
@@ -23,8 +23,28 @@ class BraceAnnimation(Scene):
         dot2 = Dot([2, 1, 0])
         line = Line(dot.get_center(), dot2.get_center()).set_color(ORANGE)
         b1 = Brace(line) # default direction is RIGHT
-        b1text = b1.get_text("Horizontal distance")
+        b1text = b1.get_text("Horizontal distance") # get_text() returns a TextMobject and put it on the direction of the brace
         b2 = Brace(line, direction=line.copy().rotate(PI/2).get_unit_vector()) # direction is aligned with line
-        b2text = b2.get_tex("x-x_1") #get_tex() returns a TexMobject
+        b2text = b2.get_tex("x-x_1") #get_tex() returns a TexMobject and put it on the direction of the brace
         self.add(line, dot, dot2, b1, b1text, b2, b2text)
+
+class VectorArrow(Scene):
+    def construct(self):
+        dot = Dot(ORIGIN)
+        arrow = Arrow(ORIGIN, [2, 2, 0], buff=0)
+        numberplane = NumberPlane()
+        self.add(numberplane, dot, arrow)
+        origintext = Text('(0,0)').next_to(dot, DOWN)
+        tip_text = Text('(2,2)').next_to(arrow, UP)
+        self.add(origintext, tip_text)
+        
+class GradientImageFromArray(Scene):
+    def construct(self):
+        n = 256
+        imageArray = np.uint8(
+            [[i * 256 / n for i in range(0, n)] for _ in range(0, n)]
+        )
+        image = ImageMobject(imageArray).scale(2)
+        image.background_rectangle = SurroundingRectangle(image, GREEN)
+        self.add(image, image.background_rectangle)
 
